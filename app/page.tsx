09a1bc2938ecbf20a533f664ca10a6c9bdc2b659
HarmanPreet-Singh-XYT <preetsinghharman27@gmail.com>
2024-10-30 11:53:51 +0530

    fix analytics not working

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Home from '@/components/Home'
 import React from 'react'
-import { Analytics } from "@vercel/analytics/react"
+
 const page = () => {
   return (
-    <>
-      <Analytics />
-      <Home/>
-    </>
+    <Home/>
   )
 }
 
