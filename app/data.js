bae2fb4085fbcd2af7bd1a46d284b34ae44a2361
Harmanpreet Singh <preetsinghharman27@gmail.com>
2025-04-02 03:18:45 +0530

    update resume link

--- app/data.js
+++ app/data.js
@@ -19,7 +19,7 @@ const about = {
   links: {
     github: "https://github.com/HarmanPreet-Singh-XYT",
     linkedin: "https://www.linkedin.com/in/harman-developer/",
-    resume:'https://drive.google.com/file/d/1PfovCVDR5h11Y6KiVM2zNUNy0bCbn2eP/view?usp=sharing',
+    resume:'https://drive.google.com/file/d/1LGKRPQxwlUgAewTPDOsV6sUXl9pIUNhX/view?usp=sharing',
   },
   email:'harmanpreetsingh@programmer.net',
   project: {
